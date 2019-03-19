28bca2166a0f3e35c011f21679d5ebddb84c1d07
pavelklimovskoy <38906858+pavelklimovskoy@users.noreply.github.com>
2019-03-19 23:48:59 +0300

    Update rotate.py

--- rotate image on 320 with step/rotate.py
+++ rotate image on 320 with step/rotate.py
@@ -1,14 +1,12 @@
 from PIL import Image
 im = Image.open('sad.png')
-n=0
+n, p, k = 0, 0, 0
 m=5
-p=0
-k=0
-while k<(360/m) :
-    k=k+1
-    p=p+m
-    n=n+1
-    s= str(n) + '.png'
+while k < (360 / m):
+    k = k + 1
+    p = p + m
+    n = n + 1 
+    s = str(n) + '.png'
     out = im.rotate(p)
     out.save(s)
     print(k)
