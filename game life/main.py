0b530ecec0a0f58666ab69fe21a8127ab5a3103a
pavelklimovskoy <38906858+pavelklimovskoy@users.noreply.github.com>
2019-03-20 20:17:35 +0300

    new main
    после рефакторинга

--- game life/main.py
+++ game life/main.py
@@ -1,78 +1,65 @@
 from tkinter import *
 
-width = 1000
-height =  800
-step = 20
-c_width = width // step
-c_height = height // step
-cells = []
-next_gen = []
-delay = 1
+screen_size_x = 1000
+screen_size_y = 700
+cell_size = 10
+size_x = screen_size_x // cell_size
+size_y = screen_size_y // cell_size
+cells = [[0 for i in range(size_y)] for j in range(size_x)]
+next_gen = [[0 for i in range(size_y)] for j in range(size_x)]
+delay = 0.01
 flag = 1
+dx = [1, 1, 1, 0, 0, -1, -1, -1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
 
 
-for x in range(c_width):
-    cells.append([])                       #тут
-    for y in range(c_height):
-        cells[x].append(0)
-
-
-for x in range(c_width):
-    next_gen.append([])                       #тут
-    for y in range(c_height):
-        next_gen[x].append(0)
-
-
-def get_n(i, j):
-    n = cells[(i - 1) % c_width][(j - 1) % c_height] + \
-        cells[(i - 1) % c_width][j] + \
-        cells[(i - 1) % c_width][(j + 1) % c_height] + \
-        cells[i][(j - 1) % c_height] + \
-        cells[i][(j + 1) % c_height] + \
-        cells[(i + 1) % c_width][(j - 1) % c_height] + \
-        cells[(i + 1) % c_width][j] + \
-        cells[(i + 1) % c_width][(j + 1) % c_height]                                #тут
+def get_n(x, y):
+    n = 0
+    for i in range(8):
+        n += cells[(x + dx[i]) % size_x][(y + dy[i]) % size_y]
     return n
 
 
+def draw_red(x, y):
+    x *= cell_size
+    y *= cell_size
+    cvs.create_rectangle(x, y, x + cell_size, y + cell_size, fill="red", outline="black")
+
+
+def draw_white(x, y):
+    x *= cell_size
+    y *= cell_size
+    cvs.create_rectangle(x, y, x + cell_size, y + cell_size, fill="white", outline="black")
+
+
 def click_l(event):
-    global step
-    x, y = event.x, event.y
-    cells[x // step][y // step] = 1
-    next_gen[x // step][y // step] = 1
-    cvs.create_rectangle(x - x % step, y - y % step, x - x % step + step, y - y % step + step, fill="red", outline="black")
+    x, y = event.x // cell_size, event.y // cell_size
+    cells[x][y] = 1
+    next_gen[x][y] = 1
+    draw_red(x, y)
 
 
 def click_r(event):
-    global step
-    x, y = event.x, event.y
-    cells[x // step][y // step] = 0
-    next_gen[x // step][y // step] = 0
-    cvs.create_rectangle(x - x % step, y - y % step, x - x % step + step, y - y % step + step, fill="white", outline="black")
+    x, y = event.x // cell_size, event.y // cell_size
+    cells[x][y] = 0
+    next_gen[x][y] = 0
+    draw_white(x, y)
 
 
-def active(x, y):
-    global step
-    x *= step
-    y *= step
-    cells[x // step][y // step] = 1
-    cvs.create_rectangle(x - x % step, y - y % step, x - x % step + step, y - y % step + step, fill="red", outline="black")
+def activation(x, y):
+    cells[x][y] = 1
+    draw_red(x, y)
 
 
-def deactive(x, y):
-    global step
-    x *= step
-    y *= step
-    cells[x // step][y // step] = 0
-    cvs.create_rectangle(x - x % step, y - y % step, x - x % step + step, y - y % step + step, fill="white", outline="black")
+def deactivation(x, y):
+    cells[x][y] = 0
+    draw_white(x, y)
 
 
-def init_setka(step):
-    global width, height
-    for i in range(0, width, step):
-        cvs.create_line(i, 0, i, height)                                #тут
-    for i in range(0, height, step):
-        cvs.create_line(0,i,width,i)
+def draw_grid():
+    for i in range(0, screen_size_x, cell_size):
+        cvs.create_line(i, 0, i, screen_size_y)
+        cvs.create_line(0, i, screen_size_x, i)
 
 
 def stop():
@@ -87,85 +74,65 @@ def start():
 
 
 def reset():
-    for x in range(c_width):
-        for y in range(c_height):                                #тут
+    for x in range(size_x):
+        for y in range(size_y):
             cells[x][y] = 0
-
-    for x in range(c_width):
-        for y in range(c_height):                                #тут
             next_gen[x][y] = 0
-
-    for x in range(c_width):
-        for y in range(c_height):
-            deactive(x, y)
+            draw_white(x, y)
 
 
 def save():
     file = open("cells", "w")
-    file.write("width = " + str(width) + "\n")
-    file.write("height = " + str(height) + "\n")
-    file.write("step = " + str(step) + "\n")
+    file.write("screen_size_x = " + str(screen_size_x) + "\n")
+    file.write("screen_size_y = " + str(screen_size_y) + "\n")
+    file.write("cell_size = " + str(cell_size) + "\n")
     for line in cells:
         for c in line:
             file.write(str(c) + " ")
         file.write("\n")
     file.write("\n")
     file.close()
-    
-    
-def simulation():
-    global  step, width, height, c_width, c_height, flag
-    if flag:
-        for i in range(c_width):
-            for j in range(c_height):
-                n = get_n(i, j)
-                if not cells[i][j]:
-                    if n == 3:
-                        next_gen[i][j] = 1
-        for i in range(c_width):
-            for j in range(c_height):
-                n = get_n(i, j)
-                if cells[i][j]:
-                    if n > 3 or n < 2:
-                        next_gen[i][j] = 0
-        for i in range(c_width):
-            for j in range(c_height):
-                if cells[i][j] != next_gen[i][j]:
-                    if next_gen[i][j]:
-                        active(i, j)
-                    else:
-                        deactive(i, j)
-    else:
-        return 0
 
-    root.after(100, simulation)
+
+def simulation():
+    if not flag:
+        return 0
+    for i in range(size_x):
+        for j in range(size_y):
+            if not cells[i][j]:
+                n = get_n(i, j)
+                if n == 3:
+                    next_gen[i][j] = 1
+    for i in range(size_x):
+        for j in range(size_y):
+            if cells[i][j]:
+                n = get_n(i, j)
+                if n > 3 or n < 2:
+                    next_gen[i][j] = 0
+    for i in range(size_x):
+        for j in range(size_y):
+            if cells[i][j] != next_gen[i][j]:
+                if next_gen[i][j]:
+                    activation(i, j)
+                else:
+                    deactivation(i, j)
+    root.after(int(delay * 1000), simulation)
 
 
 root = Tk()
-root.title("game \"life\" ")
-root.geometry(str(width)+"x"+str(height))
+root.title('game "life"')
+root.geometry(str(screen_size_x) + "x" + str(screen_size_y))
 root.resizable(width=False, height=False)
 root.configure(background='white')
-
-cvs = Canvas(root, width=width, height=height, bg="#fff")
+cvs = Canvas(root, width=screen_size_x, height=screen_size_y, bg="white")
 cvs.pack(side=TOP)
-
-root.bind('<Button-1>', click_l)
-root.bind('<Button-3>', click_r)
-
 panel = Menu(root,  background='white', activebackground='red', activeforeground='white')
 root.config(menu=panel)
-
-game_menu = Menu(panel, tearoff=0,  background='white', foreground='black', activebackground='red', activeforeground='white')
-
+root.bind('<Button-1>', click_l)
+root.bind('<Button-3>', click_r)
 panel.add_command(label='Start', command=start)
 panel.add_command(label='Stop', command=stop)
 panel.add_command(label='Reset', command=reset)
 panel.add_command(label='Save', command=save)
-
-init_setka(step)
-
-print(cells)
-
+draw_grid()
 root.mainloop()
-
